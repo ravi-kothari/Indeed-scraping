5f071d3bd186f072cbb347efa5614261e90346f5
Ravi Kothari <ravikoth@iu.edu>
2016-10-16 11:58:33 -0700

    R code

---
+++ Project_3.R
@@ -0,0 +1,71 @@
+library(RCurl)
+library(XML)
+library(rvest)
+library(knitr)
+library(dplyr)
+library(stringr)
+
+#parse the URL
+
+theURL <- "http://www.indeed.com/jobs?q=%22data+scientist%22&l=san+francisco"
+
+
+scrape_indeed_links <- function(page_data){
+
+jobLinks <- page_data %>%
+  html_nodes(xpath='//*[contains(concat( " ", @class, " " ), concat( " ", "jobtitle", " " ))]//*[contains(concat( " ", @class, " " ), concat( " ", "turnstileLink", " " ))]
+') %>%
+  html_attr("href")
+
+#is.character(jobLinks)
+#length(jobLinks)
+
+for (i in c(1:length(jobLinks))){
+  jobLinks[i] <- paste("http://www.indeed.com", jobLinks[i], sep = "")
+}
+
+linksdf <- data.frame(JobLinks=jobLinks)
+return(linksdf)
+}
+
+
+#get the total number of pages
+pages<- page_data %>%
+  html_nodes(xpath='//*[(@id = "searchCount")]') %>%
+  html_text()
+
+# regex to convert it into numbers
+results <- str_extract(pages,"of \\d+")
+results <- as.numeric(str_extract(results,"\\d+"))
+
+# need this conversion for indeed website and how they name the page numbers.
+page_no = results/10;
+page_no = floor(page_no) #number of pages
+page_no_url = page_no*10 #for URL string
+
+
+# Loop through all the pages to get the job postings URL:
+
+output <- vector(mode="character", length=page_no)
+for (i in c(1:page_no)){
+  Sys.sleep(1)
+  page_no_url = (i-1)*10
+  pageURL <- paste(theURL,"&start=",page_no_url, sep="")
+  page_data <- read_html(pageURL)
+  linksdf<-rbind(linksdf,scrape_indeed_links(page_data))
+}
+
+
+
+
+
+
+
+
+#not required but just in case to understand.
+jobLinks_title <- page_data %>%
+  html_nodes(xpath='//*[contains(concat( " ", @class, " " ), concat( " ", "turnstileLink", " " ))]') %>%
+  html_text()
+#ignore this part.
+
+
